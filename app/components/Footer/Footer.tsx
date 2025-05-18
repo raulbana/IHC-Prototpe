04a6e6e92e7d876eef0223b56a6748d6b53a9414
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-05-18 16:07:43 -0300

    feat: add footer logos

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import Image from 'next/image';
+import LogoAcessoAInformacao from '@/app/assets/acesso-a-informacao.svg';
+import LogoGovernoFederal from '@/app/assets/governo-federal.svg';
 
 export default function Footer() {
   return (
@@ -10,15 +13,23 @@ export default function Footer() {
       <p>Praça Santos Andrade, 50 | Centro | CEP: 80.020-300 | Curitiba/PR</p>
       <p>Contato: seadip@ufpr.br</p>
       <div className="flex justify-center space-x-4 mt-4 p-4">
-        <Link href="https://www.instagram.com" aria-label="Instagram">
+        <Link href="https://www.instagram.com/cipead" aria-label="Instagram" target="_blank">
           Instagram
         </Link>
-        <Link href="https://www.youtube.com" aria-label="YouTube">
+        <Link href="https://www.youtube.com/channel/UCpdgtJ711ZSg029yKwyjzhA/featured" aria-label="YouTube" target="_blank">
           YouTube
         </Link>
-        <Link href="mailto:seadip@ufpr.br" aria-label="E-mail">
+        <Link href="mailto:seadip@ufpr.br" aria-label="E-mail" target="_blank">
           E-mail
         </Link>
+      </div>
+      <div className = "flex justify-evenly p-4">
+        <a href="https://www.gov.br/acessoainformacao/pt-br" target="_blank">
+          <Image src={LogoAcessoAInformacao} alt="Logo Acesso à Informação" width={120} height={100}/>
+        </a>
+        <a href="https://www.gov.br/pt-br" className="" target="_blank">
+          <Image src={LogoGovernoFederal} alt="Logo Governo Federal" width={120} height={100}/>
+        </a>
       </div>
     </footer>
   );
